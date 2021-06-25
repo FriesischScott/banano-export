688761c0f4f93808864ba465e7bda3ebf2dfac7c
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2021-06-25 02:54:08 +0200

    Fix history store

--- src/stores/history.store.ts
+++ src/stores/history.store.ts
@@ -1,19 +1,19 @@
 import { writable, Writable } from 'svelte/store';
 import { address } from './address.store';
 
-const api: string = "https://api-beta.banano.cc";
-
 export const history: Writable<Transaction[]> = writable([]);
 
 address.subscribe(value => {
     if (value != "") {
-        fetch(api, {
+        fetch("https://banano-api-proxy.herokuapp.com/banano", {
             method: "POST",
             body: JSON.stringify({
                 action: "account_history",
                 account: value,
                 count: -1
             })
-        }).then((res) => res.json()).then(json => history.set(json));
+        }).then((res) => res.json()).then(json => {
+            history.set(json.history)
+        });
     }
 });
